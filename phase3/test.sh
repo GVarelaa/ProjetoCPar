d7fa0865aad4b4813fe76af29f77c5b5ed9c8e53
Gabriela Cunha <73352341+gabrielasfc@users.noreply.github.com>
2024-01-10 00:34:59 +0000

    Update test.sh

--- phase3/test.sh
+++ phase3/test.sh
@@ -1,27 +1,44 @@
 #!/bin/bash
-#SBATCH --time=5:00
-#SBATCH --partition=cpar
-#SBATCH --constraint=k20
-
 
 module load gcc/7.2.0
 module load cuda/11.3.1
 
-# Nome do arquivo do programa CUDA
-FILE="src/MD.cu"
-ORIGINAL_VALUE="const int N=5000;"
+file_path="src/MD.cu"
 
-VALORES_N=(1000 5000 10000 15000 20000 25000 30000 35000 40000 45000 50000)
+cp $file_path "${file_path}.bak"
 
-for N in "${VALORES_N[@]}"
-do
-    sed -i "s/$ORIGINAL_VALUE.*/const int N=$N;/" $FILE
+values=(1000 5000 10000 15000 20000 25000 30000 35000 40000 45000 50000)
+threads_per_block=(256)
+repetitions=3
 
-    make
+echo "Iteration Count;Number of Atoms;Threads Per Block;Real Time; User Time; Sys Time" >  results.csv
 
-    sbatch run.sh
+for n in "${values[@]}"; do
+    for t in "${threads_per_block[@]}"; do
+        echo "Running arguments: N=$n , THREADS_PER_BLOCK:$t"
 
+        make clean
+
+        sed -i "s/const int N=.*/const int N=$n;/" $file_path
+
+        make
+
+        for ((i=1; i<=repetitions; i++)); do
+            echo "Running iteration $i"
+            
+            make run
+            
+            mv slurm*.out temp.txt
+
+            real=$(tail -n 3 temp.txt | head -n 1 | awk '{print $2}' | sed -E 's/([0-9]+)m([0-9]+\.[0-9]+)s/\1\2/')
+            user=$(tail -n 2 temp.txt | head -n 1 | awk '{print $2}' | sed -E 's/([0-9]+)m([0-9]+\.[0-9]+)s/\1\2/')
+            sys=$(tail -n 1 temp.txt | head -n 1 | awk '{print $2}' | sed -E 's/([0-9]+)m([0-9]+\.[0-9]+)s/\1\2/')
+            
+            echo "$i;$n;$t;$real;$user;$sys" >> results.csv
+
+            rm temp.txt
+        done
+    done
 done
 
-# Restaura o valor original de N no arquivo
-sed -i "s/const int N=$N.*/$ORIGINAL_VALUE/" $FILE
+mv "${file_path}.bak" $file_path
